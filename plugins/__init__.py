90b80b0aba71003a6ca1c81a0dabc99e88f4fff7
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2025-12-01 21:24:59 +0100

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,11 @@
-# Просто создаёт папку как пакет
+import os
+import importlib
+
+def load_plugins(bot):
+    plugin_dir = os.path.dirname(__file__)
+    for file in os.listdir(plugin_dir):
+        if file.endswith(".py") and file != "__init__.py":
+            modulename = file[:-3]
+            module = importlib.import_module(f"plugins.{modulename}")
+            if hasattr(module, "register"):
+                module.register(bot)
