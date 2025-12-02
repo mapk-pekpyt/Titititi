54e09af7ffaa77f6c6d5abc97dadff75972ed1d8
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2025-12-02 20:37:53 +0100

    Update sisi.py

--- plugins/sisi.py
+++ plugins/sisi.py
@@ -1,32 +1,8 @@
-import json
-import os
-import random
-from datetime import datetime, timezone, timedelta
+import json, os
+from .common import german_time, weighted_random, get_name
 
 TRIGGER = "/sisi"
 FILE = "data/sisi.json"
-
-# Германия (Берлин) UTC+1 / UTC+2
-def german_time():
-    return datetime.now(timezone.utc).astimezone()
-
-def get_name(user):
-    if user.username:
-        return "@" + user.username
-    if user.first_name and user.last_name:
-        return f"{user.first_name} {user.last_name}"
-    return user.first_name or "Безымянный"
-
-# вероятность приращения
-def weighted_random():
-    r = random.randint(1, 100)
-
-    if r <= 65:              # 65% шанса — норм рост 1–5
-        return random.randint(1, 5)
-    elif r <= 80:            # 15%
-        return random.randint(6, 10)
-    else:                    # 20% — ноль или уменьшение
-        return random.randint(-10, 0)
 
 def load():
     if not os.path.exists(FILE):
@@ -39,7 +15,7 @@ def save(data):
         json.dump(data, f, indent=2)
 
 def handle(bot, message):
-    if not message.text or not message.text.lower().startswith(TRIGGER):
+    if not message.text or not message.text.startswith(TRIGGER):
         return
 
     user = message.from_user
@@ -47,33 +23,29 @@ def handle(bot, message):
     name = get_name(user)
 
     today = german_time().strftime("%Y-%m-%d")
-
     data = load()
 
     if uid not in data:
         data[uid] = {"size": 0, "last": "2000-01-01", "name": name}
 
-    # ежедневное изменение
     if data[uid]["last"] != today:
         delta = weighted_random()
-
         if data[uid]["size"] + delta < 0:
             delta = -data[uid]["size"]
 
         data[uid]["size"] += delta
         data[uid]["last"] = today
         data[uid]["name"] = name
-
         save(data)
 
         bot.reply_to(message,
             f"{name} 🎀\n"
-            f"Твой размер груди изменился на {delta}.\n"
-            f"Теперь размер: {data[uid]['size']}"
+            f"Твоя грудь изменилась на {delta}\n"
+            f"Теперь размер груди: {data[uid]['size']}"
         )
         return
 
-    # уже играл сегодня
+    # уже играла сегодня
     bot.reply_to(message,
         f"{name} 🎀 ты уже играла сегодня!\n"
         f"Твой текущий размер груди: {data[uid]['size']}"
