991f890c285e4242ae4bed07a4bbb74ef36ccf2c
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2025-12-02 02:27:17 +0100

    Create triggers.py

---
+++ triggers.py
@@ -0,0 +1,8 @@
+TRIGGERS = {
+    "/sisi": "sisi",
+    "/hui": "hui",
+    "/klitot": "klitor",
+    "/mut": "mut",
+    "/top": "top_plugin",
+    "/price": "mut",  # команда для изменения цены у плагина mut
+}
