40b542dafd3ff48ee534b630f7066a004a8f5d46
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2025-12-03 03:45:25 +0100

    Update sisi.py

--- plugins/sisi.py
+++ plugins/sisi.py
@@ -1,18 +1,30 @@
-# plugins/sisi.py
-
-from plugins.common import weighted_random, get_name
-from plugins.top_plugin import add_stat
+from plugins.common import weighted_random, get_name, german_date
+from plugins.top_plugin import ensure_user, update_stat, update_date, was_today
 
 
 def handle(bot, message):
     user = message.from_user
     name = get_name(user)
+    chat = message.chat.id
+
+    data = ensure_user(chat, user)
+
+    if was_today(chat, user, "last_sisi"):
+        current = data[str(chat)][str(user.id)]["sisi"]
+        return bot.reply_to(
+            message,
+            f"{name}, шалунишка ты мой, думал не замечу? "
+            f"Ты уже играл сегодня и твои вишенки сейчас {current} размера 😳🍒"
+        )
 
     delta = weighted_random()
+    update_stat(chat, user, "sisi", delta)
+    update_date(chat, user, "last_sisi")
 
-    add_stat(message.chat.id, user, "sisi_size", delta)
+    new_size = data[str(chat)][str(user.id)]["sisi"] + delta
 
     bot.reply_to(
         message,
-        f"{name}, твой новый размер сисек: {delta:+} размера 😳",
+        f"{name}, твои сисечки выросли на {delta:+}, "
+        f"теперь твоя грудь {new_size} размера 😳🍒"
     )
