5cd49388c2b4dc4cb6d191aa4e2a276c4344e513
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2025-12-04 05:20:09 +0100

    Update triggers.py

--- triggers.py
+++ triggers.py
@@ -1,26 +1,40 @@
 TRIGGERS = {
+    # ==== SISI ====
     "/sisi": "sisi",
     "/sisi@Sisititibot": "sisi",
 
+    "/busts": "sisi",
+    "/busts@Sisititibot": "sisi",
+
+    "/bustprice": "sisi",
+    "/bustprice@Sisititibot": "sisi",
+
+    # ==== HUI ====
     "/hui": "hui",
     "/hui@Sisititibot": "hui",
 
+    "/busth": "hui",
+    "/busth@Sisititibot": "hui",
+
+    # ==== KLITOR ====
     "/klitor": "klitor",
     "/klitor@Sisititibot": "klitor",
 
+    "/bustk": "klitor",
+    "/bustk@Sisititibot": "klitor",
+
+    # ==== MUT ====
     "/mut": "mut",
     "/mut@Sisititibot": "mut",
 
     "/price": "mut",
     "/price@Sisititibot": "mut",
 
+    # ==== TOP ====
     "/top": "top_plugin",
     "/top@Sisititibot": "top_plugin",
 
+    # ==== KTO ====
     "/kto": "kto",
     "/kto@Sisititibot": "kto",
-    "/busts": "sisi",
-    "/busth": "hui",
-    "/bustk": "klitor",
-    "/bustprice": "sisi",   # можно любой — он общий
 }
