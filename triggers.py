3095bdac08a6fd2496a36ad1e5cb1508e5c27ac4
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2025-12-11 22:41:19 +0100

    Update triggers.py

--- triggers.py
+++ triggers.py
@@ -4,69 +4,83 @@ TRIGGERS = {
     # ==== SISI ====
     "/sisi": "sisi",
     "/sisi@Sisititibot": "sisi",
-
+    "сиськи": "sisi",
     "/boosts": "sisi",
     "/boosts@Sisititibot": "sisi",
-
+    "бусты": "sisi",
     "/boostprice": "bust_price",
     "/boostprice@Sisititibot": "bust_price",
+    "цена_бустов": "bust_price",
 
     # ==== HUI ====
     "/hui": "hui",
     "/hui@Sisititibot": "hui",
-
+    "хуй": "hui",
     "/boosth": "hui",
     "/boosth@Sisititibot": "hui",
+    "бустх": "hui",
 
     # ==== KLITOR ====
     "/klitor": "klitor",
     "/klitor@Sisititibot": "klitor",
-
+    "клитор": "klitor",
     "/boostk": "klitor",
     "/boostk@Sisititibot": "klitor",
+    "бустк": "klitor",
 
     # ==== MUT ====
     "/mut": "mut",
     "/mut@Sisititibot": "mut",
-
+    "мут": "mut",
     "/price": "mut",
     "/price@Sisititibot": "mut",
+    "цена_мута": "mut",
 
     # ==== TOP ====
     "/top": "top_plugin",
     "/top@Sisititibot": "top_plugin",
+    "топ": "top_plugin",
 
     # ==== KTO ====
     "/kto": "kto",
     "/kto@Sisititibot": "kto",
+    "кто": "kto",
     
-    #loto
+    # ==== LOTO ====
     "/loto": "loto",
     "/loto@Sisititibot": "loto",
+    "лото": "loto",
     
     "/lotoprice": "loto",
     "/lotoprice@Sisititibot": "loto",
+    "цена_лото": "loto",
     
     "/gift": "loto",
     "/gift@Sisititibot": "loto",
+    "подарок": "loto",
     
+    # ==== MINUS ====
     "/minuss": "minus",
     "/minuss@Sisititibot": "minus",
-    
+    "минусс": "minus",
     "/minush": "minus",
     "/minush@Sisititibot": "minus",
-    
+    "минусх": "minus",
     "/minusk": "minus",
     "/minusk@Sisititibot": "minus",
-    
+    "минуск": "minus",
+
+    # ==== SAY ====
     "/say": "say",
     "/say@Sisititibot": "say",
-   
-     #реклама 
+    "скажи": "say",
+
+    # ==== ADS ====
+    "/priser": "ads",
+    "/priser@Sisititibot": "ads",
+    "цена_рекламы": "ads",
     
-     "/priser": "ads",
-    "/priser@Sisititibot": "/ads",
-    
-     "/buy_ads": "ads",
-    "/buy_ads@Sisititibot": "/ads",
+    "/buy_ads": "ads",
+    "/buy_ads@Sisititibot": "ads",
+    "купить_рекламу": "ads",
 }
